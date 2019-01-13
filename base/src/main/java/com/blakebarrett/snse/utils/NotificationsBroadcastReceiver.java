57ef58c61937cace16293d1367042682fc9ea453
Blake Barrett <blake@blakebarrett.net>
2019-01-12 19:46:00 -0800

    This _should_ fix the crash on boot issue. (Hopefully).

--- base/src/main/java/com/blakebarrett/snse/utils/NotificationsBroadcastReceiver.java
+++ base/src/main/java/com/blakebarrett/snse/utils/NotificationsBroadcastReceiver.java
@@ -9,11 +9,6 @@ public class NotificationsBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
         Log.d("snse.broadcastreceiver", "onReceive: " + String.valueOf(intent));
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            final Intent service = new Intent(context, NotificationService.class);
-            context.startService(service);
-        } else {
-            NotificationUtils.handleIntent(context, intent);
-        }
+        NotificationUtils.handleIntent(context.getApplicationContext(), intent);
     }
 }
